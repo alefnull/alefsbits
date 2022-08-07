867ab52e2ae56242306af7d3123a738434cbd2f4
alefnull <nullalef@gmail.com>
2022-08-06 23:15:07 -0400

    move range select to menu with unipolar option

--- src/simplexandhold.cpp
+++ src/simplexandhold.cpp
@@ -5,7 +5,6 @@
 
 struct Simplexandhold : Module {
 	enum ParamId {
-		RANGE_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -22,13 +21,13 @@ struct Simplexandhold : Module {
 
 	SimplexNoise noise;
 	dsp::SchmittTrigger trigger[MAX_POLY];
-	double last_sample[MAX_POLY] = {0.0};
-	double x[MAX_POLY] = {0.0};
-	double range = 1.0;
+	bool unipolar = false;
+	float last_sample[MAX_POLY] = {0.0};
+	float x[MAX_POLY] = {0.0};
+	float range = 1.0;
 
 	Simplexandhold() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configSwitch(RANGE_PARAM, 0, 2, 0, "noise range", {"1", "3", "5"});
 		configInput(TRIGGER_INPUT, "trigger");
 		configOutput(SAMPLE_OUTPUT, "sample");
 		noise.init();
@@ -37,23 +36,17 @@ struct Simplexandhold : Module {
 	void process(const ProcessArgs& args) override {
 		int chans = std::max(1, inputs[TRIGGER_INPUT].getChannels());
 		outputs[SAMPLE_OUTPUT].setChannels(chans);
-		// get the desired output range (-1.0 to 1.0, -3.0 to 3.0, -5.0 to 5.0)
-		double range_param = params[RANGE_PARAM].getValue();
-		if (range_param == 0.0) {
-			range = 1.0;
-		}
-		else if (range_param == 1.0) {
-			range = 3.0;
-		}
-		else if (range_param == 2.0) {
-			range = 5.0;
-		}
 		for (int c = 0; c < chans; c++) {
 			if (trigger[c].process(inputs[TRIGGER_INPUT].getVoltage(c))) {
 				last_sample[c] = noise.noise(x[c], 0.0) * range;
 			}
-			x[c] += args.sampleTime;
-			outputs[SAMPLE_OUTPUT].setVoltage(last_sample[c], c);
+			x[c] += 0.001;
+			if (unipolar) {
+				outputs[SAMPLE_OUTPUT].setVoltage((last_sample[c] + range) / 2.0, c);
+			}
+			else {
+				outputs[SAMPLE_OUTPUT].setVoltage(last_sample[c], c);
+			}
 		}
 	}
 };
@@ -69,13 +62,28 @@ struct SimplexandholdWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<CKSSThreeHorizontal>(mm2px(Vec(10.16, 78.539)), module, Simplexandhold::RANGE_PARAM));
-
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16, 35.937)), module, Simplexandhold::TRIGGER_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.16, 100.446)), module, Simplexandhold::SAMPLE_OUTPUT));
 	}
-};
 
+	void appendContextMenu(Menu* menu) override {
+		Simplexandhold* module = dynamic_cast<Simplexandhold*>(this->module);
+		assert(module);
+
+		menu->addChild(new MenuSeparator());
+		// add a submenu to choose the range, between
+		// +/- 1V, +/- 3V, +/- 5V, +/- 10V
+		menu->addChild(createSubmenuItem("Range", "", [=](Menu* menu) {
+			Menu* rangeMenu = new Menu();
+			rangeMenu->addChild(createMenuItem("-/+ 1v", CHECKMARK(module->range == 1), [module]() { module->range = 1; }));
+			rangeMenu->addChild(createMenuItem("-/+ 3v", CHECKMARK(module->range == 3), [module]() { module->range = 3; }));
+			rangeMenu->addChild(createMenuItem("-/+ 5v", CHECKMARK(module->range == 5), [module]() { module->range = 5; }));
+			rangeMenu->addChild(createMenuItem("-/+ 10v", CHECKMARK(module->range == 10), [module]() { module->range = 10; }));
+			menu->addChild(rangeMenu);
+		}));
+		menu->addChild(createMenuItem("Unipolar", CHECKMARK(module->unipolar), [module]() { module->unipolar = !module->unipolar; }));
+	}
+};
 
 Model* modelSimplexandhold = createModel<Simplexandhold, SimplexandholdWidget>("simplexandhold");
