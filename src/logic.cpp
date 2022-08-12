189c4ef407d7369ac977cb04d5427945ccbd11c7
alefnull <nullalef@gmail.com>
2022-08-12 17:31:18 -0400

    logic: add lights

--- src/logic.cpp
+++ src/logic.cpp
@@ -21,6 +21,12 @@ struct Logic : Module {
 		OUTPUTS_LEN
 	};
 	enum LightId {
+		AND_LIGHT,
+		OR_LIGHT,
+		XOR_LIGHT,
+		NAND_LIGHT,
+		NOR_LIGHT,
+		XNOR_LIGHT,
 		LIGHTS_LEN
 	};
 
@@ -34,6 +40,12 @@ struct Logic : Module {
 		configOutput(NAND_OUTPUT, "");
 		configOutput(NOR_OUTPUT, "");
 		configOutput(XNOR_OUTPUT, "");
+		configLight(AND_LIGHT, "");
+		configLight(OR_LIGHT, "");
+		configLight(XOR_LIGHT, "");
+		configLight(NAND_LIGHT, "");
+		configLight(NOR_LIGHT, "");
+		configLight(XNOR_LIGHT, "");
 	}
 
 	void process(const ProcessArgs& args) override {
@@ -46,12 +58,24 @@ struct Logic : Module {
 		for (int c = 0; c < channels; c++) {
 			bool a = inputs[A_INPUT].getVoltage(c) > 0.0;
 			bool b = inputs[B_INPUT].getVoltage(c) > 0.0;
-			outputs[AND_OUTPUT].setVoltage(a && b ? 10.0 : 0.0, c);
-			outputs[OR_OUTPUT].setVoltage(a || b ? 10.0 : 0.0, c);
-			outputs[XOR_OUTPUT].setVoltage(a ^ b ? 10.0 : 0.0, c);
-			outputs[NAND_OUTPUT].setVoltage(!(a && b) ? 10.0 : 0.0, c);
-			outputs[NOR_OUTPUT].setVoltage(!(a || b) ? 10.0 : 0.0, c);
-			outputs[XNOR_OUTPUT].setVoltage(!(a ^ b) ? 10.0 : 0.0, c);
+			bool _and = a && b;
+			bool _or = a || b;
+			bool _xor = a ^ b;
+			bool _nand = !_and;
+			bool _nor = !_or;
+			bool _xnor = !_xor;
+			outputs[AND_OUTPUT].setVoltage(_and ? 10.0 : 0.0, c);
+			lights[AND_LIGHT].setBrightness(_and ? 1.0 : 0.0);
+			outputs[OR_OUTPUT].setVoltage(_or ? 10.0 : 0.0, c);
+			lights[OR_LIGHT].setBrightness(_or ? 1.0 : 0.0);
+			outputs[XOR_OUTPUT].setVoltage(_xor ? 10.0 : 0.0, c);
+			lights[XOR_LIGHT].setBrightness(_xor ? 1.0 : 0.0);
+			outputs[NAND_OUTPUT].setVoltage(_nand ? 10.0 : 0.0, c);
+			lights[NAND_LIGHT].setBrightness(_nand ? 1.0 : 0.0);
+			outputs[NOR_OUTPUT].setVoltage(_nor ? 10.0 : 0.0, c);
+			lights[NOR_LIGHT].setBrightness(_nor ? 1.0 : 0.0);
+			outputs[XNOR_OUTPUT].setVoltage(_xnor ? 10.0 : 0.0, c);
+			lights[XNOR_LIGHT].setBrightness(_xnor ? 1.0 : 0.0);
 		}
 	}
 };
@@ -76,6 +100,13 @@ struct LogicWidget : ModuleWidget {
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.285, 84.639)), module, Logic::NAND_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.285, 96.023)), module, Logic::NOR_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(8.285, 106.963)), module, Logic::XNOR_OUTPUT));
+
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 47.547)), module, Logic::AND_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 58.079)), module, Logic::OR_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 69.563)), module, Logic::XOR_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 80.639)), module, Logic::NAND_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 92.023)), module, Logic::NOR_LIGHT));
+		addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(12.285, 102.963)), module, Logic::XNOR_LIGHT));
 	}
 };
 
