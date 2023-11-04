d64a24b405702befa4e4a61ff0965ae0c62484b2
alefnull <nullalef@gmail.com>
2023-11-04 15:21:10 -0400

    polyrand: rework to actually get desired result

--- src/polyrand.cpp
+++ src/polyrand.cpp
@@ -21,9 +21,8 @@ struct Polyrand : Module {
 		LIGHTS_LEN
 	};
 
-	dsp::SchmittTrigger trigger[MAX_POLY];
-	float last_value[MAX_POLY] = { 0.f };
-	int current_channel[MAX_POLY] = { 0 };
+	dsp::SchmittTrigger trigger;
+	int out_chan = 0;
 
 	Polyrand() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
@@ -36,32 +35,21 @@ struct Polyrand : Module {
 	}
 
 	void process(const ProcessArgs& args) override {
-		int trig_channels = inputs[TRIGGER_INPUT].isConnected() ? inputs[TRIGGER_INPUT].getChannels() : 1;
-		if (trig_channels > MAX_POLY) {
-			trig_channels = MAX_POLY;
+		int channels = inputs[POLY_INPUT].isConnected() ? inputs[POLY_INPUT].getChannels() : 1;
+		if (channels > MAX_POLY) {
+			channels = MAX_POLY;
 		}
-		outputs[RANDOM_OUTPUT].setChannels(trig_channels);
 
 		if (inputs[TRIGGER_INPUT].isConnected() &&
-				inputs[POLY_INPUT].isConnected() &&
-				outputs[RANDOM_OUTPUT].isConnected()) {
+			inputs[POLY_INPUT].isConnected() &&
+			outputs[RANDOM_OUTPUT].isConnected()) {
 
-			int poly_channels = inputs[POLY_INPUT].getChannels();
-			if (poly_channels > MAX_POLY) {
-				poly_channels = MAX_POLY;
-			}
-			for (int i = 0; i < trig_channels; i++) {
-				if (trigger[i].process(inputs[TRIGGER_INPUT].getVoltage(i))) {
-					int chan = random::u32() % poly_channels;
-					current_channel[i] = chan;
-				}
-				last_value[i] = inputs[POLY_INPUT].getVoltage(current_channel[i]);
-			}
-
-			for (int i = 0; i < trig_channels; i++) {
-				outputs[RANDOM_OUTPUT].setVoltage(last_value[i], i);
+			if (trigger.process(inputs[TRIGGER_INPUT].getVoltage())) {
+				out_chan = random::u32() % channels;
 			}
 		}
+
+		outputs[RANDOM_OUTPUT].setVoltage(inputs[POLY_INPUT].getVoltage(out_chan));
 	}
 };
 
